e9b30538fe2e6686403886a7fb3aefeba74255fb
BeardRubyBlue <sander.sisoev@gmail.com>
2024-01-29 12:43:04 +0300

    firstPR

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿class Program {
+	static void Main() {
+		Console.WriteLine("Введите значение x:");
+		int x = int.Parse(Console.ReadLine());
+		double result = x - 10 * Math.Sin(x)+ Math.Abs(Math.Pow(x, 4) - Math.Pow(x, 5));
+		Console.WriteLine(result);
+	}
+}
